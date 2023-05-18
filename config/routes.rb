69c61fa68aca23e8b801e48037a097b209715f76
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2023-05-18 02:10:38 -0300

    CRUD finalizado

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       get 'recipes/index'
       post 'recipes/create'
-      get '/show/:id', to: 'recipes#destroy'
-      delete 'destroy/:id', to: 'recipes#destroy'
+      get '/show/:id', to: 'recipes#show'
+      delete '/destroy/:id', to: 'recipes#destroy'
     end
   end
   root 'homepage#index'
